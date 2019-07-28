646b2e2468001b3c0be61c3e1ce2f831fcba1c8b
Jake Goodman <jakemgoodman@gmail.com>
2019-07-27 19:19:36 -0700

    Initial commit

---
+++ intro/Assets/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() => print("hello world, love Jake");
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
